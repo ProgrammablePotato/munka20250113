c9aabb9bc3d4b59e995ce65f01cebe58dcdca4ce
ProgrammablePotato <tamas.mihelik@gmail.com>
2025-01-13 10:55:57 +0100

    iuhvfehvfuhrf erferiferiofher

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UploadFormComponent } from './upload-form/upload-form.component';
+import { UploadListComponent } from './upload-list/upload-list.component';
+import { FormsModule } from '@angular/forms';
+import { environments } from './environments';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadFormComponent,
+    UploadListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environments.firebaseConfig),
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+  ],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/upload-list/upload-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { UploadService } from '../upload.service';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-upload-list',
+  templateUrl: './upload-list.component.html',
+  styleUrl: './upload-list.component.css'
+})
+export class UploadListComponent {
+files:any
+
+constructor(
+  private uploadService:UploadService
+){
+  this.getFiles()
+  // this.uploadService.getFiles().snapshotChanges().pipe(
+  //   map(
+  //     (changes:any)=>changes.map((c:any)=> ({key:c.payload.key,...c.payload.val() }))
+  //   )
+  // ).subscribe(
+  //   (files:any)=>this.files=files
+  // )
+}
+
+getFiles() {
+  this.uploadService.getExpressFiles().subscribe(
+    (res)=>this.files = res
+  )
+}
+
+deleteFile(file:any){
+  this.uploadService.deleteFile(file)
+}
+
+}
